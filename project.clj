c028b64103e89ec0cd86fba82d4b63a65a53cf9f
Egg Syntax <egg@novonon.com>
2015-12-06 21:40:08 -0500

    dependencies

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [cljsjs/mathbox "0.0.1-0"]
+                 [org.clojure/core.async "0.2.374"]
                  [org.clojure/clojurescript "1.7.170" :classifier "aot"
                   :exclusion [org.clojure/data.json]]
                  [org.clojure/data.json "0.2.6" :classifier "aot"]]
